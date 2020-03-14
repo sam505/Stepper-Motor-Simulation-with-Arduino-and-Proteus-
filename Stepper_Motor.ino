8a33c98b08fcad76c7aa102a0665397eeae0083a
sam505 <47660356+sam505@users.noreply.github.com>
2020-03-14 16:15:40 +0300

    Add files via upload

---
+++ Stepper_Motor.ino
@@ -0,0 +1,37 @@
+
+void setup() {
+   pinMode (4,OUTPUT);
+   pinMode (5,OUTPUT);
+   pinMode (6,OUTPUT);
+   pinMode (7,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite (4,HIGH);
+  digitalWrite (5,LOW);
+  digitalWrite (6,LOW);
+  digitalWrite (7,LOW);
+  delay(100);
+
+  
+
+  digitalWrite (4,LOW);
+  digitalWrite (5,HIGH);
+  digitalWrite (6,LOW);
+  digitalWrite (7,LOW);
+  delay(100);
+
+  digitalWrite (4,LOW);
+  digitalWrite (5,LOW);
+  digitalWrite (6,HIGH);
+  digitalWrite (7,LOW);
+  delay(100);
+  
+  digitalWrite (4,LOW);
+  digitalWrite (5,LOW);
+  digitalWrite (6,LOW);
+  digitalWrite (7,HIGH);
+  delay(100);
+
+}
